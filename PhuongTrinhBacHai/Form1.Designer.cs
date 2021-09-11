80136d68ae921cb053c5509dcf783694b6bea585
quoctuan <tranquoctuan31012001@gmail.com>
2021-09-11 11:52:36 +0700

    giai pt bac hai

---
+++ PhuongTrinhBacHai/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace PhuongTrinhBacHai
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_c = new System.Windows.Forms.TextBox();
+            this.txt_b = new System.Windows.Forms.TextBox();
+            this.txt_a = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txt_x2 = new System.Windows.Forms.TextBox();
+            this.txt_x1 = new System.Windows.Forms.TextBox();
+            this.lb_x2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.exit = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.calc = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Aqua;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txt_c);
+            this.panel1.Controls.Add(this.txt_b);
+            this.panel1.Controls.Add(this.txt_a);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(41, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(720, 146);
+            this.panel1.TabIndex = 0;
+            this.panel1.Tag = "";
+            // 
+            // txt_c
+            // 
+            this.txt_c.BackColor = System.Drawing.Color.LightGreen;
+            this.txt_c.ForeColor = System.Drawing.Color.Red;
+            this.txt_c.Location = new System.Drawing.Point(163, 107);
+            this.txt_c.Name = "txt_c";
+            this.txt_c.Size = new System.Drawing.Size(259, 22);
+            this.txt_c.TabIndex = 8;
+            // 
+            // txt_b
+            // 
+            this.txt_b.BackColor = System.Drawing.Color.LightGreen;
+            this.txt_b.ForeColor = System.Drawing.Color.Red;
+            this.txt_b.Location = new System.Drawing.Point(163, 68);
+            this.txt_b.Name = "txt_b";
+            this.txt_b.Size = new System.Drawing.Size(259, 22);
+            this.txt_b.TabIndex = 7;
+            // 
+            // txt_a
+            // 
+            this.txt_a.BackColor = System.Drawing.Color.PaleGreen;
+            this.txt_a.ForeColor = System.Drawing.Color.Red;
+            this.txt_a.Location = new System.Drawing.Point(163, 26);
+            this.txt_a.Name = "txt_a";
+            this.txt_a.Size = new System.Drawing.Size(259, 22);
+            this.txt_a.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(25, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "A";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Moccasin;
+            this.panel2.Controls.Add(this.txt_x2);
+            this.panel2.Controls.Add(this.txt_x1);
+            this.panel2.Controls.Add(this.lb_x2);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(41, 248);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(720, 90);
+            this.panel2.TabIndex = 1;
+            // 
+            // txt_x2
+            // 
+            this.txt_x2.Location = new System.Drawing.Point(163, 57);
+            this.txt_x2.Name = "txt_x2";
+            this.txt_x2.Size = new System.Drawing.Size(259, 22);
+            this.txt_x2.TabIndex = 8;
+            // 
+            // txt_x1
+            // 
+            this.txt_x1.Location = new System.Drawing.Point(163, 15);
+            this.txt_x1.Name = "txt_x1";
+            this.txt_x1.Size = new System.Drawing.Size(259, 22);
+            this.txt_x1.TabIndex = 7;
+            // 
+            // lb_x2
+            // 
+            this.lb_x2.AutoSize = true;
+            this.lb_x2.Location = new System.Drawing.Point(114, 57);
+            this.lb_x2.Name = "lb_x2";
+            this.lb_x2.Size = new System.Drawing.Size(25, 17);
+            this.lb_x2.TabIndex = 5;
+            this.lb_x2.Text = "X2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(114, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "X1";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.PowderBlue;
+            this.panel3.Controls.Add(this.exit);
+            this.panel3.Controls.Add(this.clear);
+            this.panel3.Controls.Add(this.calc);
+            this.panel3.Location = new System.Drawing.Point(43, 387);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(718, 145);
+            this.panel3.TabIndex = 2;
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.LightCoral;
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.Location = new System.Drawing.Point(536, 73);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(119, 32);
+            this.exit.TabIndex = 11;
+            this.exit.Text = "Thoát";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.YellowGreen;
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(273, 70);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(125, 32);
+            this.clear.TabIndex = 10;
+            this.clear.Text = "Bỏ qua";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // calc
+            // 
+            this.calc.BackColor = System.Drawing.Color.Pink;
+            this.calc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calc.Location = new System.Drawing.Point(56, 73);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(104, 32);
+            this.calc.TabIndex = 9;
+            this.calc.Text = "Tính";
+            this.calc.UseVisualStyleBackColor = false;
+            this.calc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lb1
+            // 
+            this.lb1.BackColor = System.Drawing.Color.Lime;
+            this.lb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.ForeColor = System.Drawing.Color.Red;
+            this.lb1.Location = new System.Drawing.Point(38, 9);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(156, 42);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "Nhập các hệ số";
+            this.lb1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Lime;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(44, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Kết quả";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Yellow;
+            this.label6.Location = new System.Drawing.Point(25, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 22);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "B";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(25, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 22);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "C";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 547);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Giải phương trình Ax^2 + bx + c = 0";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.TextBox txt_c;
+        private System.Windows.Forms.TextBox txt_b;
+        private System.Windows.Forms.TextBox txt_a;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_x2;
+        private System.Windows.Forms.TextBox txt_x1;
+        private System.Windows.Forms.Label lb_x2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button calc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ PhuongTrinhBacHai/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhuongTrinhBacHai
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            load();
+
+        }
+        protected void load()
+        {
+            reset();
+            this.panel2.Enabled = false;
+        }
+        protected void Handler()
+        {
+            double a;
+            double b;
+            double c;
+            if (double.TryParse(this.txt_a.Text, out a) && double.TryParse(this.txt_b.Text, out b)
+                && double.TryParse(this.txt_c.Text, out c))
+            {
+                this.panel2.Enabled = true;
+                double D = b * b - 4 * a * c;
+                if (D < 0)
+                {
+                    this.txt_x1.Text = "Phương trình vô nghiệm";
+                    this.txt_x2.Enabled = false;
+                    this.lb_x2.Enabled = false;
+                }
+                else
+                    if (D == 0)
+                {
+                    this.txt_x1.Text = ((-b) / (2 * a)).ToString();
+                    this.txt_x2.Enabled = false;
+                    this.lb_x2.Enabled = false;
+                }
+                else
+                {
+                    this.txt_x1.Enabled = true;
+                    this.txt_x2.Enabled = true;
+                    this.txt_x1.Text = ((-b - Math.Sqrt(D)) / (2 * a)).ToString();
+                    this.txt_x2.Text = ((-b + Math.Sqrt(D)) / (2 * a)).ToString();
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bạn nhập sai");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Handler();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            load();
+        }
+        protected void reset()
+        {
+            this.txt_a.ResetText();
+            this.txt_b.ResetText();
+            this.txt_c.ResetText();
+            this.txt_x1.ResetText();
+            this.txt_x2.ResetText();
+            this.txt_a.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
